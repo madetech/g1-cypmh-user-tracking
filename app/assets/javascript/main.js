283af5482cf8d70b98beeed1026ae21dfafdc8f0
Emma Blott <blott23@gmail.com>
2021-01-27 11:24:44 +0000

    refactoring main.js

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,57 +1,84 @@
 // ES6 or Vanilla JavaScript
-// ES6 or Vanilla JavaScript
-window.onload = window.onunload = function analytics(event) {
-    if (!navigator.sendBeacon) return;
 
-    let url = "/tracking";
-    // Create the data to send
-    let sessionId = document.cookie.split("=")[1]
-    // Send the beacon
-    let status = navigator.sendBeacon(url, JSON.stringify({sessionId: sessionId, type: event.type, location: location.href, time: Date()}));
-};
-// var elements = document.getElementsByClassName("track-me");
-// console.log(elements);
-// for (var i = 0; i < elements.length; i++) {
-//   elements[i].onclick = function(event) {
-//     console.log("clicked");
-//     console.log(event)
-//   };
-// };
+// Define page props
+if(!document.cookie){
+  document.cookie = `id=${Math.floor(Math.random() * 900000) + 100000}`;
+}
+// Define global variables 
+const sessionId = document.cookie.split("=")[1]
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+
+const createOnScreenArray = (className) => {
+  let elements = document.getElementsByClassName(className);
+
+  let isOnscreen = []
+  Array.from(elements).forEach((item) => {
+      isOnscreen = [...isOnscreen, {onscreen: isInViewport(item), element: item}]
+  });
+  return isOnscreen
 }
 
-var elements = document.getElementsByClassName("track-me");
-// console.log(elements);
-// for (var i = 0; i < elements.length; i++) {
-//   elements[i].onclick = function(event) {
-//     console.log("clicked");
-//     console.log(event)
-//   };
-// };
+let isOnscreen = createOnScreenArray("track-me")
 
-let isOnscreen = []
-// for (var i = 0; i < elements.length; i++) {
-//   console.log(elements)
-//   isOnscreen = [...isOnscreen, {onscreen: isInViewport(elements[i]), element: elements[i]}]
-// };
-Array.from(elements).forEach(function(item) {
-    isOnscreen = [...isOnscreen, {onscreen: isInViewport(item), element: item}]
-});
-// isOnscreen = elements.map(element_obj => ({onscreen: false, element: element_obj}))//[{id:1, onscreen:false, element:element}]
+// Global Functions
+const sendToTracking = (data) => {
+  if (!navigator.sendBeacon) return;
+
+  let url = "/tracking";
+
+  let status = navigator.sendBeacon(url, JSON.stringify(data));
+};
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Event functions
+window.onunload = function (event) {
+  let data = {sessionId: sessionId, type: event.type, location: location.href, time: Date()};
+  sendToTracking(data)
+}
+
+window.onload = function (event) {
+  let data = {sessionId: sessionId, type: event.type, location: location.href, time: Date()};
+  sendToTracking(data)
+
+
+  isOnscreen.forEach(item => {
+    let result = item.onscreen
+    let visibility;
+    if (result === true) {
+        visibility = "on screen";
+    } else {
+        visibility = "off screen";
+    }
+    if (!navigator.sendBeacon) return;
+    let url = "/tracking";
+    // Create the data to send
+    let sessionId = document.cookie.split("=")[1];
+    // Send the beacon
+    let visibilityData = {visibility: visibility, objectInnerText: item.element.innerText}
+    let status = navigator.sendBeacon(url, JSON.stringify({sessionId: sessionId, type: "scroll", location: location.href, eventData:visibilityData,  time: Date()}));
+  });
+};
+
+
+
+
+
+
 window.onscroll = function scroll_two(event) {
     isOnscreen.forEach(item => {
         let result = isInViewport(item.element)
         let visibility;
         if (item.onscreen !== result) {
-            // console.log(item.element);
+            
             if (result === true) {
                 visibility = "on screen";
             } else {
@@ -69,24 +96,24 @@ window.onscroll = function scroll_two(event) {
     });
 }
 
-window.onload = function scroll_three(event) {
-    isOnscreen.forEach(item => {
-        let result = item.onscreen
-        let visibility;
-        if (result === true) {
-            visibility = "on screen";
-        } else {
-            visibility = "off screen";
-        }
-        if (!navigator.sendBeacon) return;
-        let url = "/tracking";
-        // Create the data to send
-        let sessionId = document.cookie.split("=")[1];
-        // Send the beacon
-        let visibilityData = {visibility: visibility, objectInnerText: item.element.innerText}
-        let status = navigator.sendBeacon(url, JSON.stringify({sessionId: sessionId, type: "scroll", location: location.href, eventData:visibilityData,  time: Date()}));
-    });
-}
+// window.onload = function scroll_three(event) {
+    // isOnscreen.forEach(item => {
+    //     let result = item.onscreen
+    //     let visibility;
+    //     if (result === true) {
+    //         visibility = "on screen";
+    //     } else {
+    //         visibility = "off screen";
+    //     }
+    //     if (!navigator.sendBeacon) return;
+    //     let url = "/tracking";
+    //     // Create the data to send
+    //     let sessionId = document.cookie.split("=")[1];
+    //     // Send the beacon
+    //     let visibilityData = {visibility: visibility, objectInnerText: item.element.innerText}
+    //     let status = navigator.sendBeacon(url, JSON.stringify({sessionId: sessionId, type: "scroll", location: location.href, eventData:visibilityData,  time: Date()}));
+    // });
+// }
 
 document.onclick = function analytics_click(event) {
     if (!navigator.sendBeacon) return;
@@ -105,8 +132,6 @@ document.onclick = function analytics_click(event) {
     }
 };
 
-if(!document.cookie){
-    document.cookie = `id=${Math.floor(Math.random() * 900000) + 100000}`;
-}
+
 
 
